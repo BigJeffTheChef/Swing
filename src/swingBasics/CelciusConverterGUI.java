174c5e682e49017c8c0bc8d0f9049590f2495dfd
BigJeffTheChef <marleygpeter@gmail.com>
2022-01-07 17:13:39 +0000

    lel

--- src/swingBasics/CelciusConverterGUI.java
+++ src/swingBasics/CelciusConverterGUI.java
@@ -1,6 +1,5 @@
 package swingBasics;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -12,11 +11,18 @@ import javax.swing.JButton;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+/**
+ * Current guide page: https://docs.oracle.com/javase/tutorial/uiswing/learn/logic.html
+ * @author Peter Marley
+ *
+ */
 public class CelciusConverterGUI extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField inputTextField;
 
 	/**
 	 * Launch the application.
@@ -40,44 +46,49 @@ public class CelciusConverterGUI extends JFrame {
 	public CelciusConverterGUI() {
 		setTitle("Celcius Converter");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 419, 170);
+		setBounds(100, 100, 184, 102);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
+		inputTextField = new JTextField();
+		inputTextField.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("New label");
+		JLabel labelC = new JLabel("C");
 		
-		JButton btnNewButton = new JButton("New button");
+		JButton buttonConvert = new JButton("Convert");
+		buttonConvert.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
+		JLabel labelF = new JLabel("F");
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(buttonConvert, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(inputTextField, Alignment.TRAILING))
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(btnNewButton)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)))
+							.addComponent(labelC, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE))
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(labelF, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)))
 					.addContainerGap(656, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel))
+						.addComponent(inputTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(labelC))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnNewButton)
-						.addComponent(lblNewLabel_1))
+						.addComponent(buttonConvert)
+						.addComponent(labelF))
 					.addContainerGap(398, Short.MAX_VALUE))
 		);
 		contentPane.setLayout(gl_contentPane);
