a28b47a99ac3924e65b2b19d9d9a97042ee14c52
BigJeffTheChef <marleygpeter@gmail.com>
2022-01-07 17:03:46 +0000

    breaking any sec i can feel it

--- src/swingBasics/CelciusConverterGUI.java
+++ src/swingBasics/CelciusConverterGUI.java
@@ -9,6 +9,9 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class CelciusConverterGUI extends JFrame {
 
@@ -37,25 +40,46 @@ public class CelciusConverterGUI extends JFrame {
 	public CelciusConverterGUI() {
 		setTitle("Celcius Converter");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 825, 491);
+		setBounds(100, 100, 419, 170);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
 		textField = new JTextField();
-		contentPane.add(textField, BorderLayout.NORTH);
-		//contentPane.ad
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("New label");
-		contentPane.add(lblNewLabel, BorderLayout.WEST);
 		
 		JButton btnNewButton = new JButton("New button");
-		contentPane.add(btnNewButton, BorderLayout.CENTER);
 		
 		JLabel lblNewLabel_1 = new JLabel("New label");
-		contentPane.add(lblNewLabel_1, BorderLayout.EAST);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(btnNewButton)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(656, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnNewButton)
+						.addComponent(lblNewLabel_1))
+					.addContainerGap(398, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
